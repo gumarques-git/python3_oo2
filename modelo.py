09af3689b9f09685d183cd7a117ff544d089a6f2
Gustavo <gumarques@gmail.com>
2019-08-26 01:32:42 +0100

    Usando Heranca

--- modelo.py
+++ modelo.py
@@ -1,49 +1,42 @@
-class Filme:
+class Programa:
+
+    def __init__(self, nome, ano):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.__likes = 0
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_like(self):
+        self.__likes += 1
+
+
+
+class Filme(Programa):
 
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
 
 
-class Serie:
+
+class Serie(Programa):
 
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
 
-    @property
-    def nome(self):
-        return self.__nome
 
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
 
 vinga = Filme('vingadores - guerra infinita', 2018, 160)
 vinga.dar_like()
