b4bf1d16faf454c92946efc5aeeac9d980a17cc5
Gustavo <gumarques@gmail.com>
2019-08-27 21:49:20 +0100

    Imprimindo usando herenca

--- modelo.py
+++ modelo.py
@@ -1,3 +1,5 @@
+# coding: UTF-8
+
 class Programa:
 
     def __init__(self, nome, ano):
@@ -40,11 +42,17 @@ class Serie(Programa):
 
 vinga = Filme('vingadores - guerra infinita', 2018, 160)
 vinga.dar_like()
-print(f'Nome: {vinga.nome} - Ano: {vinga.ano} - Duracao: {vinga.duracao} - Likes: {vinga.likes}')
+#print(f'Nome: {vinga.nome} - Ano: {vinga.ano} - Duracao: {vinga.duracao} - Likes: {vinga.likes}')
 
 
 atlan = Serie('atlanta', 2018, 2)
 atlan.dar_like()
 atlan.dar_like()
-atlan.nome = 'atlanda 2'
-print(f'Nome: {atlan.nome} - Ano: {atlan.ano} - Temporadas: {atlan.temporadas} - Likes: {atlan.likes}')
+#print(f'Nome: {atlan.nome} - Ano: {atlan.ano} - Temporadas: {atlan.temporadas} - Likes: {atlan.likes}')
+
+
+filmes_e_series = [vinga, atlan]
+
+for programa in filmes_e_series:
+    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
+    print(f'Nome: {programa.nome} || Detalhes: {detalhes} || Likes: {programa.likes}')
