67dead4a762fbb8279680362c2637e6ab7a1685c
Gustavo <gumarques@gmail.com>
2019-08-29 02:08:34 +0100

    Usando Mixins

--- funcionario.py
+++ funcionario.py
@@ -3,6 +3,9 @@
 
 
 class Funcionario:
+    def __init__(self, nome):
+        self.nome = nome.title()
+
     def registra_horas(self, horas):
         print('Horas registradas...')
 
@@ -26,6 +29,12 @@ class Alura(Funcionario):
         print('Mostrando perguntas não respondidas do fórum')
 
 
+# Chamamos estas classes pequenas, cujos objetos nem precisam ser instanciados, de Mixins.
+# Elas são bastante utilizadas em Python no caso de precisarmos compartilhar algum
+# comportamento que não é o mais importante desta classe.
+class Hipster:
+    def __str__(self):
+        return f'Hipster, {self.nome}'
 
 class Junior(Alura):
     pass
@@ -33,11 +42,14 @@ class Junior(Alura):
 class Pleno(Alura, Caelum):
     pass
 
+class Senior(Alura, Caelum, Hipster):
+    pass
 
-jose = Junior()
+
+jose = Junior('Jose')
 jose.busca_perguntas_sem_resposta()
 
-luan = Pleno()
+luan = Pleno('Luan')
 luan.busca_perguntas_sem_resposta()
 luan.busca_cursos_do_mes()
 
@@ -52,3 +64,8 @@ luan.mostrar_tarefas()
 # super classe duplicada deixando somente mais a frente na hierarquia, Ex:
 # class Pleno(Alura, Caelum):
 # Pleno > Alura > Caelum > Funcionario
+
+
+# Testando Heranca usando Mixins
+gus = Senior('Gus')
+print(gus)
