77be2962f6c1829a7a79344a0e6cdf9551b66185
Gustavo <gumarques@gmail.com>
2019-08-29 01:59:35 +0100

    Algoritmo chamado MRO (Method Resolution Order)

--- funcionario.py
+++ funcionario.py
@@ -19,8 +19,8 @@ class Caelum(Funcionario):
 
 
 class Alura(Funcionario):
-    def mostrar_tarefas(self):
-        print('Fez muita coisa, Alurete!')
+    # def mostrar_tarefas(self):
+    #     print('Fez muita coisa, Alurete!')
 
     def busca_perguntas_sem_resposta(self):
         print('Mostrando perguntas não respondidas do fórum')
@@ -42,3 +42,13 @@ luan.busca_perguntas_sem_resposta()
 luan.busca_cursos_do_mes()
 
 luan.mostrar_tarefas()
+
+# O algoritmo chamado MRO (Method Resolution Order) do
+# Python Busca os metodos da Heranca das classes das esquerda para a direita, Ex:
+# class Pleno(Alura, Caelum):
+# Pleno > Alura > Funcionario > Caelum > Funcionario
+
+# Porem se ha classes que herdam das mesma super classe, ele remove a
+# super classe duplicada deixando somente mais a frente na hierarquia, Ex:
+# class Pleno(Alura, Caelum):
+# Pleno > Alura > Caelum > Funcionario
